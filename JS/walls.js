ae44db3b4002913e44a24c7116d3b391c86e1bf6
Christophe Protin <protin.christophe@gmail.com>
2018-12-29 06:45:09 +0100

    adds all walls level 1

--- JS/walls.js
+++ JS/walls.js
@@ -36,6 +36,82 @@ c.lineTo(320,400);
 c.fill();
 
 //shape 3
+c.beginPath();
+c.fillStyle = 'rgb(83,160,243)';
+c.moveTo(520,480);
+c.lineTo(520,520);
+c.lineTo(80,520);
+c.lineTo(80,360);
+c.lineTo(120,360);
+c.lineTo(120,480);
+c.lineTo(520,480);
+c.fill();
+
+//shape 4
+c.beginPath();
+c.fillStyle = 'rgb(83,160,243)';
+c.moveTo(200,80);
+c.lineTo(200,120);
+c.lineTo(120,120);
+c.lineTo(120,270);
+c.lineTo(80,270);
+c.lineTo(80,80);
+c.lineTo(280,80);
+c.fill();
+
+//shape 5
+c.beginPath();
+c.fillStyle = 'rgb(11,186,14)';
+c.moveTo(80,270);
+c.lineTo(40,270);
+c.lineTo(40,230);
+c.lineTo(80,230);
+c.lineTo(80,270);
+c.fill();
+
+//shape 6
+c.beginPath();
+c.fillStyle = 'rgb(83,160,243)';
+c.moveTo(560,480,);
+c.lineTo(680,480);
+c.lineTo(680,360);
+c.lineTo(720,360);
+c.lineTo(720,520);
+c.lineTo(560,520);
+c.lineTo(560,480);
+c.fill();
+
+//shape 7
+c.beginPath();
+c.fillStyle = 'rgb(11,186,14)';
+c.moveTo(720,280);
+c.lineTo(760,280);
+c.lineTo(760,240);
+c.lineTo(720,240);
+c.lineTo(720,280);
+c.fill();
+
+//shape 8
+c.beginPath();
+c.fillStyle = 'rgb(83,160,243)';
+c.moveTo(720,280);
+c.lineTo(680,280);
+c.lineTo(680,120);
+c.lineTo(640,120);
+c.lineTo(640,80);
+c.lineTo(720,80);
+c.lineTo(720,280);
+c.fill();
+
+//shape 9
+c.beginPath();
+c.fillStyle = 'rgb(83,160,243)';
+c.moveTo(600,120);
+c.lineTo(560,120);
+c.lineTo(560,80);
+c.lineTo(600,80);
+c.lineTo(600,120);
+c.fill();
 
 //final shape
 c.beginPath();
