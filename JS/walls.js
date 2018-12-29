0d3d4261b2110063b4294753d4810855d5f723f5
Christophe Protin <protin.christophe@gmail.com>
2018-12-29 06:00:27 +0100

    adds final shape

--- JS/walls.js
+++ JS/walls.js
@@ -19,3 +19,39 @@ c.lineTo(280,280);
 c.lineTo(280,200);
 c.fill();
 
+//Shape 2
+c.beginPath();
+c.fillStyle = 'rgb(212,65,62)';
+c.moveTo(320,400);
+c.lineTo(320,430);
+c.lineTo(200,430);
+c.lineTo(200,80);
+c.lineTo(560,80);
+c.lineTo(560,520);
+c.lineTo(520,520);
+c.lineTo(520,120);
+c.lineTo(240,120);
+c.lineTo(240,400);
+c.lineTo(320,400);
+c.fill();
+
+//shape 3
+
+//final shape
+c.beginPath();
+c.fillStyle = 'rgb(247,141,27)';
+c.moveTo(0,0);
+c.lineTo(800,0);
+c.lineTo(800,600);
+c.lineTo(0,600);
+c.lineTo(0,200);
+c.lineTo(40,200);
+c.lineTo(40,560);
+c.lineTo(760,560);
+c.lineTo(760,40);
+c.lineTo(40,40);
+c.lineTo(40,160);
+c.lineTo(0,160);
+c.lineTo(0,0);
+c.fill();
+
